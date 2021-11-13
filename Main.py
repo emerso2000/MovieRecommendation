af996a9aa405c69940332137864290accee4d4bb
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2021-11-13 15:46:15 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+movies = pd.read_csv(r'C:\Users\moore\PycharmProjects\Hackathon Project\anime\animes.csv')
+
+ratings = pd.read_csv(r'C:\Users\moore\PycharmProjects\Hackathon Project\anime\ratings.csv')
+
+class Movies:
+    for line in movies:
+        input_list = input_list.append(line)
+        
+
+
